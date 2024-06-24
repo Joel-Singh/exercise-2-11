60dba64ec60a91e464ca51f97b20dce37e73e6c8
Joel Singh <joelsingh788@gmail.com>
2024-06-24 13:00:16 -0400

    Do runs with multiple parameters

--- main.py
+++ main.py
@@ -16,11 +16,19 @@ def getSingleListOfAverages(listContainingListsOfAverages: list[list[float]]):
         singleListOfAverages.append(average)
     return singleListOfAverages
 
-firstRun = run(useIncrementalEstimateCalculation=True, chanceToSelectRandomly=0.1)
-secondRun = run(useIncrementalEstimateCalculation=True, chanceToSelectRandomly=0.1)
+averageRewardsOverTheLast100000Steps: list[float] = []
 
-print("averageRewardOverTheLast100000Steps is " + str(firstRun["averageRewardOverTheLast100000Steps"]))
-plt.plot([firstRun['averageRewardOverTheLast100000Steps'], secondRun['averageRewardOverTheLast100000Steps']], 'r')
+PARAMETERS: Final = [1/128, 1/64, 1/32, 1/16, 1/8, 1/4, 1/2, 1, 2, 4]
+for index,parameter in enumerate(PARAMETERS):
+    averageRewardsOverTheLast100000Steps.append(
+        run(
+            useIncrementalEstimateCalculation=False,
+            chanceToSelectRandomly=parameter
+        )['averageRewardOverTheLast100000Steps']
+    )
+    print(str(round((index + 1) / len(PARAMETERS), 2) * 100) + "%")
+
+plt.plot(PARAMETERS, averageRewardsOverTheLast100000Steps, 'r')
 
 plt.ylabel("Average reward over the last 100,000 steps")
 plt.xlabel("Parameters (Epsilon, alpha, c, optimistic estimate)")
