039e9245d956bf1b2ec69f0cdb2a69eca7519dca
Joel Singh <joelsingh788@gmail.com>
2024-06-24 13:08:33 -0400

    Remove unused function

--- main.py
+++ main.py
@@ -2,16 +2,6 @@ from typing import Final
 from DoRun import run
 import matplotlib.pyplot as plt
 
-def getSingleListOfAverages(listContainingListsOfAverages: list[list[float]]):
-    singleListOfAverages: list[float] = []
-    for i in range(len(listContainingListsOfAverages[0])):
-        average = 0
-        for _,listOfAverages in enumerate(listContainingListsOfAverages):
-            average += listOfAverages[i]
-        average /= len(listContainingListsOfAverages)
-        singleListOfAverages.append(average)
-    return singleListOfAverages
-
 averageRewardsOverTheLast100000Steps: list[float] = []
 
 PARAMETERS: Final = [1/128, 1/64, 1/32, 1/16, 1/8, 1/4, 1/2, 1, 2, 4]
