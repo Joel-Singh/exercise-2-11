5a72498d5b7efddebc8f8fccd85c1837e8f1aace
Joel Singh <joelsingh788@gmail.com>
2024-06-26 17:10:40 -0400

    Move epsilonGreedyAverageRewards into dict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import numpy as np
-from typing import Final
+from typing import Final, TypedDict
 from DoRun import run
 import matplotlib.pyplot as plt
 from concurrent import futures
@@ -8,7 +8,18 @@ from concurrent import futures
 PARAMETERS: Final = [1/128, 1/64, 1/32, 1/16, 1/8, 1/4, 1/2, 1, 2, 4]
 PARAMETERS_AS_STRING: Final = ["1/128", "1/64", "1/32", "1/16", "1/8", "1/4", "1/2", "1", "2", "4"]
 
-averageRewardsOverTheLast100000StepsAsFutures: list[futures.Future] = []
+class banditAlgorithmAverageRewardsAsFutures(TypedDict):
+    epsilonGreedy: list[futures.Future]
+    gradient: list[futures.Future]
+    greedyWithOptimisticInitialization: list[futures.Future]
+    upperConfidenceBound: list[futures.Future]
+
+averageRewardsAsFutures: banditAlgorithmAverageRewardsAsFutures = {
+    "epsilonGreedy": [],
+    "gradient": [],
+    "greedyWithOptimisticInitialization": [],
+    "upperConfidenceBound": [],
+}
 
 with futures.ProcessPoolExecutor(max_workers=os.cpu_count()) as ex:
     def runAndGetAverageReward(index):
@@ -18,9 +29,9 @@ with futures.ProcessPoolExecutor(max_workers=os.cpu_count()) as ex:
         )['averageRewardOverTheLast100000Steps']
 
     for i,_ in enumerate(PARAMETERS):
-        averageRewardsOverTheLast100000StepsAsFutures.append(ex.submit(runAndGetAverageReward, i))
+        averageRewardsAsFutures["epsilonGreedy"].append(ex.submit(runAndGetAverageReward, i))
 
-plt.plot([future.result() for future in averageRewardsOverTheLast100000StepsAsFutures], 'r')
+plt.plot([future.result() for future in averageRewardsAsFutures["epsilonGreedy"]], 'r')
 plt.xticks(np.arange(len(PARAMETERS)), PARAMETERS_AS_STRING)
 
 plt.ylabel("Average reward over the last 100,000 steps")
