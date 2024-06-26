e6f38bad3375e632bf3d7e5c0fd23080875e5e10
Joel Singh <joelsingh788@gmail.com>
2024-06-26 00:26:17 -0400

    Concurrently do runs

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 from typing import Final
 from DoRun import run
 import matplotlib.pyplot as plt
+from concurrent import futures
 
 averageRewardsOverTheLast100000Steps: list[float] = []
 
 PARAMETERS: Final = [1/128, 1/64, 1/32, 1/16, 1/8, 1/4, 1/2, 1, 2, 4]
-for index,parameter in enumerate(PARAMETERS):
-    averageRewardsOverTheLast100000Steps.insert(
-        index,
-        run(
-            useIncrementalEstimateCalculation=False,
-            chanceToSelectRandomly=parameter
-        )['averageRewardOverTheLast100000Steps']
-    )
-    print(str(round((index + 1) / len(PARAMETERS), 2) * 100) + "%")
+
+with futures.ProcessPoolExecutor(max_workers=4) as ex:
+    def runAndInsertAverageReward(index, parameter):
+        averageRewardsOverTheLast100000Steps.insert(
+            index,
+            run(
+                useIncrementalEstimateCalculation=False,
+                chanceToSelectRandomly=parameter
+            )['averageRewardOverTheLast100000Steps']
+        )
+        print("done")
+    for index,parameter in enumerate(PARAMETERS):
+        ex.submit(runAndInsertAverageReward, index, parameter)
 
 plt.plot(PARAMETERS, averageRewardsOverTheLast100000Steps, 'r')
 
