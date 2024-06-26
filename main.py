7db492bb27fd80cfd62e3f8e6db7780dc99306e3
Joel Singh <joelsingh788@gmail.com>
2024-06-26 16:46:30 -0400

    Evenly space ticks

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
+import numpy as np
 from typing import Final
 from DoRun import run
 import matplotlib.pyplot as plt
 from concurrent import futures
 
 PARAMETERS: Final = [1/128, 1/64, 1/32, 1/16, 1/8, 1/4, 1/2, 1, 2, 4]
+PARAMETERS_AS_STRING: Final = ["1/128", "1/64", "1/32", "1/16", "1/8", "1/4", "1/2", "1", "2", "4"]
 
 averageRewardsOverTheLast100000StepsAsFutures: list[futures.Future] = []
 
@@ -18,8 +20,8 @@ with futures.ProcessPoolExecutor(max_workers=os.cpu_count()) as ex:
     for i,_ in enumerate(PARAMETERS):
         averageRewardsOverTheLast100000StepsAsFutures.append(ex.submit(runAndGetAverageReward, i))
 
-plt.plot(PARAMETERS, [future.result() for future in averageRewardsOverTheLast100000StepsAsFutures], 'r')
-plt.xticks(PARAMETERS)
+plt.plot([future.result() for future in averageRewardsOverTheLast100000StepsAsFutures], 'r')
+plt.xticks(np.arange(len(PARAMETERS)), PARAMETERS_AS_STRING)
 
 plt.ylabel("Average reward over the last 100,000 steps")
 plt.xlabel("Parameters (Epsilon, alpha, c, optimistic estimate)")
