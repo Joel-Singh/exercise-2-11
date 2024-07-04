138f12832e13f8ab5d88f559dc9b1e7d94f36259
Joel Singh <joelsingh788@gmail.com>
2024-07-03 23:01:01 -0400

    Refactor runGradient into a chooseAction

--- DoRun.py
+++ DoRun.py
@@ -11,6 +11,9 @@ def calculateNewAverageIncrementally(oldAverage, nextValue, numberOfValues):
 def calculateNewAverageWithStepSizeParameter(oldAverage, nextValue, stepSizeParameter):
     return oldAverage + (stepSizeParameter) * (nextValue - oldAverage)
 
+def getReward(action: int, trueValues: list[float]):
+    return random.normalvariate(trueValues[action], 1)
+
 # Returns reward
 def getChooseActionGreedy(
     useIncrementalEstimateCalculation: bool = False,
@@ -39,9 +42,6 @@ def getChooseActionGreedy(
         highestEstimateActions = getHighestEstimateActions()
         return random.choice(highestEstimateActions)
 
-    def getReward(action: int, trueValues: list[float]):
-        return random.normalvariate(trueValues[action], 1)
-
     STEP_SIZE_PARAMETER: Final = 0.1
     def updateEstimate(action: int, reward: float, currentStep: int):
         if (estimates[action] is None):
@@ -64,101 +64,39 @@ def getChooseActionGreedy(
 
     return chooseAction
 
-def runGradient(stepSizeParameter: float):
-    NUMBER_OF_STEPS: Final = 2 * 10**6
+def getChooseActionGradient(stepSizeParameter: float) -> Callable[[int, list[float]], float]:
+    preferences: list[float] = [0 for _ in range(10)]
+    averageReward = 0
 
-    averageReward: float = 0
+    # Page 37
+    def getProbabilities():
+        total: float = 0
+        for preference in preferences:
+            total += math.e**(preference)
 
-    class Lever(TypedDict):
-        preference: float
-        getReward: Callable[[], float]
-        takeRandomWalk: Callable[[], None]
-        getTrueValue: Callable[[], float]
+        return [math.e**preference / total for preference in preferences]
 
-    currentRandomNumber: int = -1
-    randomWalkNumbers: np.ndarray = np.random.normal(0, 0.01, NUMBER_OF_STEPS * 10)
-    def getRandomWalkNumber():
-        nonlocal currentRandomNumber
-        nonlocal randomWalkNumbers
+    def updateAverageReward(reward, currentStep):
+        nonlocal averageReward
+        averageReward = calculateNewAverageIncrementally(averageReward, reward, currentStep + 1)
 
-        currentRandomNumber = currentRandomNumber + 1
-        return randomWalkNumbers[currentRandomNumber]
-
-    def createLever() -> Lever:
-        trueValue = random.normalvariate(0, 1)
-        def takeRandomWalk():
-            nonlocal trueValue
-            trueValue += getRandomWalkNumber()
-        return {
-            "preference": 0,
-            "getReward": lambda: random.normalvariate(trueValue, 1),
-            "takeRandomWalk": takeRandomWalk,
-            "getTrueValue": lambda: trueValue
-        }
-
-    levers = [
-        createLever(),
-        createLever(),
-        createLever(),
-        createLever(),
-        createLever(),
-        createLever(),
-        createLever(),
-        createLever(),
-        createLever(),
-        createLever(),
-    ]
-
-    averageRewardOverTheLast100000Steps: float = 0
-
-    for i in range(NUMBER_OF_STEPS):
+    def updatePreferences(chosenAction: int, reward: float):
+        probabilities = getProbabilities()
         # Page 37
-        def getProbabilities():
-            total: float = 0
-            for lever in levers:
-                total += math.e**(lever["preference"])
-
-            return [math.e**lever["preference"] / total for lever in levers]
-
-        def chooseLever():
-            return random.choices(levers, getProbabilities())[0]
-
-        def updatePreferences(chosenLever: Lever, reward: float):
-            probabilities = getProbabilities()
-            # Page 37
-            for index,lever in enumerate(levers):
-                if (lever is chosenLever):
-                    lever["preference"] = lever["preference"] + stepSizeParameter * (reward - averageReward) * (1 - probabilities[index])
-
-                
-        def updateAverageRewardOverTheLast100000Steps(reward):
-            nonlocal averageRewardOverTheLast100000Steps
-            # dividing by 2 incase I lower the NUMBER_OF_STEPS for testing
-            if (i < (NUMBER_OF_STEPS / 2)):
-                return
-
-            if (i == (NUMBER_OF_STEPS / 2)):
-                averageRewardOverTheLast100000Steps = reward
+        for i,preference in enumerate(preferences):
+            if (i == chosenAction):
+                preferences[i] = preference + stepSizeParameter * (reward - averageReward) * (1 - probabilities[i])
             else:
-                averageRewardOverTheLast100000Steps = calculateNewAverageIncrementally(averageRewardOverTheLast100000Steps, reward, (i - 10**6) + 1)
+                preferences[i] = preference - stepSizeParameter * (reward - averageReward) * probabilities[i]
 
-        def updateAverageReward(reward):
-            nonlocal averageReward
-            averageReward = calculateNewAverageIncrementally(averageReward, reward, i + 1)
+    def chooseAction(trueValues, currentStep):
+        action = random.choices([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], getProbabilities())[0] 
+        reward: float = getReward(action, trueValues)
+        updateAverageReward(reward, currentStep)
+        updatePreferences(action, reward)
+        return reward
 
-        def walkLevers():
-            for lever in levers:
-                lever["takeRandomWalk"]()
-
-        lever = chooseLever()
-        reward = lever['getReward']()
-
-        updatePreferences(lever, reward)
-        updateAverageReward(reward)
-        updateAverageRewardOverTheLast100000Steps(reward)
-        walkLevers()
-
-    return averageRewardOverTheLast100000Steps 
+    return chooseAction 
 
 def run(chooseAction: Callable[[int, list[float]], float]):
     NUMBER_OF_STEPS: Final = 2 * 10**6
